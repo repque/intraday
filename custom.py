12829e73aadb0dd5b0f98aa79c37b14a78cc1feb
repque <repque@gmail.com>
2020-04-10 18:02:17 -0400

    Update custom.py
    fixed some minor code style issues ...

--- custom.py
+++ custom.py
@@ -1,61 +1,61 @@
 from __future__ import print_function
 import datetime
 import logging
-import settings.appconfig as set
-import session as s
-from td.orders import Order, OrderLeg
-from td.enums import ORDER_SESSION, DURATION, ORDER_INSTRUCTIONS, ORDER_ASSET_TYPE, ORDER_STRATEGY_TYPE, ORDER_TYPE, QUANTITY_TYPE
+import settings.appconfig as config
+from   session import Session
+from   td.orders import Order, OrderLeg
+import td.enums
+import time
 import json
 
 
 def get_data_point( symbol ):
-    
     ''' Pull current data point here from data provider for the symbol '''    
-    session = s.Session().session    
+    
+    session = Session().session    
     # get real data here
     price = session.get_quotes(instruments=symbol)[symbol]['lastPrice']
     time_stamp = datetime.datetime.now()
-    logging.debug( 'Get Quote from API: {},{}'.format( price, time_stamp ) )
+    logging.debug( '{}: {},{}'.format( symbol, time_stamp, price ) )
         
     return time_stamp, price
 
 def submit_order( symbol, qty, is_entry ):
-    
     ''' Place order through TD API '''
-    session = s.Session().session
-    account_id = set.td['account_id']
+    
+    session = Session().session
+    account_id = config.td['account_id']
     fill_price = 0
     
-    logging.debug( 'Executing signal: {},{},{}'.format( symbol, qty, is_entry ) )
-    send_order = set.td['live_orders']
     instruction = 'buy' if is_entry else 'sell'
+    logging.debug( 'Executing signal: {} {} {}'.format( instruction, qty, symbol ) )
+    send_order = config.td['live_orders']
 
     new_order = Order()
-    new_order.order_type(order_type = ORDER_TYPE.MARKET)
-    new_order.order_session(session = ORDER_SESSION.NORMAL)
-    new_order.order_duration(duration = DURATION.DAY)
-    new_order.order_strategy_type(order_strategy_type = ORDER_STRATEGY_TYPE.SINGLE)
+    new_order.order_type(order_type = td.enums.ORDER_TYPE.MARKET)
+    new_order.order_session(session = td.enums.ORDER_SESSION.NORMAL)
+    new_order.order_duration(duration = td.enums.DURATION.DAY)
+    new_order.order_strategy_type(order_strategy_type = td.enums.ORDER_STRATEGY_TYPE.SINGLE)
 
     new_order_leg = OrderLeg()
-    if (instruction == 'buy'):
-        new_order_leg.order_leg_instruction(instruction = ORDER_INSTRUCTIONS.BUY)
+    if ( is_entry ):
+        new_order_leg.order_leg_instruction(instruction = td.enums.ORDER_INSTRUCTIONS.BUY)
     else:
-        new_order_leg.order_leg_instruction(instruction = ORDER_INSTRUCTIONS.SELL)
+        new_order_leg.order_leg_instruction(instruction = td.enums.ORDER_INSTRUCTIONS.SELL)
 
-    new_order_leg.order_leg_asset(asset_type = ORDER_ASSET_TYPE.EQUITY, symbol = symbol)
-    new_order_leg.order_leg_quantity_type(quantity_type = QUANTITY_TYPE.SHARES)
+    new_order_leg.order_leg_asset(asset_type = td.enums.ORDER_ASSET_TYPE.EQUITY, symbol = symbol)
+    new_order_leg.order_leg_quantity_type(quantity_type = td.enums.QUANTITY_TYPE.SHARES)
     new_order_leg.order_leg_quantity(quantity=int(qty))
     new_order.add_order_leg(order_leg = new_order_leg)
     if (send_order):
-        session.place_order(account = account_id, order= new_order)
-        fill_price, time_stamp = get_filled_price(account_id,symbol)    
+        session.place_order( account = account_id, order = new_order )
+        fill_price = get_filled_price( account_id, symbol )    
 
     return fill_price
 
 def get_filled_price (account, symbol):
-
     ''' Find latest filled order for symbol '''
-    session = s.Session().session
+    session = Session().session
     time.sleep(2)
     today = datetime.date.today()
     # Get all today's fulfilled orders
@@ -63,7 +63,9 @@ def get_filled_price (account, symbol):
     # Get last filled order for symbol
     last_symbol_order = [i for i in orders if i['orderLegCollection'][0]['instrument']['symbol'] == symbol][-1]        
     fill_price = last_symbol_order['orderActivityCollection'][0]['executionLegs'][0]['price']
-    fill_time = last_symbol_order['orderActivityCollection'][0]['executionLegs'][0]['time']
-    time_stamp = datetime.datetime.strptime( fill_time, '%Y-%m-%dT%H:%M:%S+0000' )
+     
+    # don't need this
+    #fill_time = last_symbol_order['orderActivityCollection'][0]['executionLegs'][0]['time']
+    #time_stamp = datetime.datetime.strptime( fill_time, '%Y-%m-%dT%H:%M:%S+0000' )
 
-    return fill_price, time_stamp
+    return fill_price
