0d8dabba5a00eb79a9640239679ca3a95a87ff4d
Stan <stan@fitnessbi.com>
2020-05-17 21:50:23 -0400

    Added day by day processing, saving and combining charts

--- app.py
+++ app.py
@@ -11,8 +11,9 @@ from   coroutines import initial_breakout, time_based, stop_loss, stop_profit
 from   data_providers import gen_csv_data, gen_time_series
 from   positions import Pnl
 import utils
+import pandas as pd
 
-def run( configs, live=False, specific_day=None, cash=25000, commission=0.01, interval=1 ):
+def run( configs, live=False, specific_day=None, cash=25000, commission=0, interval=1, daily_charts=True, charts_folder='.\\charts\\testing' ):
     ''' main event loop 
 
         if 'live' mode is False, we're testing and running against previously recorded data.
@@ -35,7 +36,7 @@ def run( configs, live=False, specific_day=None, cash=25000, commission=0.01, in
         if not active_strategies:
             logging.debug( 'All Done!' )
             logging.info( pnl.get_report() )
-            utils.plot( pnl, live, specific_day is None )
+            utils.plot( pnl, daily_charts, specific_day is None, charts_folder )
             break # we're done
         
         for strategy in active_strategies:
@@ -62,4 +63,32 @@ if __name__ == '__main__':
 
     # to replay specific day, set the argument to datetime instance:
     # for example: specific_day = datetime.datetime( 2020, 4, 2 )
-    run( configs, live = False, specific_day = None )
+    live = False
+    specific_day = None #datetime.datetime( 2020, 4, 1 )
+    day_by_day = True #Change if you want to process whole set instead of day by day
+
+    if not(live):
+        charts_folder = '.\\charts\\testing'
+        if not(specific_day):
+            if (day_by_day):
+                #Process one day at a time, export and combine charts                
+                df = pd.read_csv('.\\data\\' + configs[0].symbol + '.csv', header=None, index_col=0)
+                df.index = pd.to_datetime(df.index, format='%Y-%m-%d %H:%M:%S')
+                df['date'] = df.index.date     
+                dates = df['date'].unique()
+                for specific_day in dates:
+                    run( configs, live, specific_day = datetime.datetime.combine(specific_day, datetime.datetime.min.time()), daily_charts = True, charts_folder = charts_folder )
+                    if len(configs) > 1:
+                        utils.combine_charts(charts_folder, combine_pattern = specific_day)                    
+                for eachconfig in configs:
+                    utils.combine_charts(charts_folder, combine_pattern = eachconfig.symbol)
+            else:
+                #Process whole set of days at once
+                run( configs, live, specific_day = None, daily_charts = True, charts_folder = charts_folder )
+        else:
+            #Process for specific day
+            run( configs, live, specific_day = specific_day, daily_charts = True, charts_folder = charts_folder )
+    else:
+        #Run live
+        charts_folder = '.\\charts\\live'
+        run( configs, live, specific_day = None, daily_charts = True, charts_folder = charts_folder )
